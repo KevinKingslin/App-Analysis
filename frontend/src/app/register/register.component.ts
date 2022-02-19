3522e0f5ef4aef69f0531c040b47b1eba3405cbc
Kevin Kingslin <kevinkingslin@gmail.com>
2022-02-19 18:07:37 +0530

    Added form validation for login page

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import {FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
