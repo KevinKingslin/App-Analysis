75306610ccdb0eae8c6940693d0f16514fea8111
Kevin Kingslin <kevinkingslin@gmail.com>
2022-03-01 18:30:19 +0530

    Merged changes from frontend branch

---
+++ frontend/src/app/register.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { json } from 'body-parser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private HttpClient: HttpClient, private router: Router) { }
+
+  PostRegisterData(username: string, email: string, password: string){
+    const headers = new HttpHeaders()
+          .set('Content-Type', 'application/json');
+
+    this.HttpClient.post('http://localhost:8000/register', { username, email, password }, {
+      headers: headers
+    }).subscribe((res) => {
+      this.router.navigate(['/index'])
+    },
+    (err) => {
+      console.log(err)
+    })
+  }
+}
