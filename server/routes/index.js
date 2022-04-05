b39ca9a4e5a34cebc6cccdd6d491f9b85bc06b1e
Arghya <fleabag@kali.Fleabag>
2022-04-05 23:29:33 +0530

    metasploit route added to the server

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,11 @@ var userRegistration = require("./register");
 var userLogin = require("./login");
 var checkAuth = require("./checkAuth");
 var nmap = require("./nmap");
+var metasploit = require("./metasploit");
 
 router.post("/register", userRegistration);
 router.post("/login", userLogin.UserLogin);
 router.post("/change-password", checkAuth, userLogin.changePassword); //checkAuth is used to secure the route
 router.post("/user/nmap", checkAuth, nmap);
-
+router.post("/user/metasploit", checkAuth, metasploit);
 module.exports = router;
